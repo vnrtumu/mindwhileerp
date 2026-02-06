228ec3092538a4e16f51bcb65a56688768398197
Your Name <you@example.com>
2026-02-06 13:25:33 +0530

    initial commit for Superadmin

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { Suspense } from 'react'
+import { createRoot } from 'react-dom/client'
+import './src/css/globals.css'
+import App from './superAdmin/App.tsx'
+import Spinner from './superAdmin/views/spinner/Spinner.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <Suspense fallback={<Spinner />}>
+    <App />
+  </Suspense>
+)
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import fs from 'fs/promises';
+import svgr from '@svgr/rollup';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'src': resolve(__dirname, './src/superAdmin'),
+      '/src': resolve(__dirname, './src/superAdmin'),
+      "@/components": resolve(__dirname, "./src/superAdmin/components"),
+      "@/assets": resolve(__dirname, "./src/superAdmin/assets"),
+      "@/hooks": resolve(__dirname, "./src/superAdmin/hooks"),
+      "@/layouts": resolve(__dirname, "./src/superAdmin/layouts"),
+      "@/routes": resolve(__dirname, "./src/superAdmin/routes"),
+      "@/store": resolve(__dirname, "./src/superAdmin/store"),
+      "@/views": resolve(__dirname, "./src/superAdmin/views"),
+      "@/utils": resolve(__dirname, "./src/superAdmin/utils"),
+    },
+  },
+  esbuild: {
+    loader: 'tsx',
+    include: /src\/.*\.tsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: 'load-js-files-as-tsx',
+          setup(build) {
+            build.onLoad(
+              { filter: /src\\.*\.js$/ },
+              async (args) => ({
+                loader: 'tsx',
+                contents: await fs.readFile(args.path, 'utf8'),
+              })
+            );
+          },
+        },
+      ],
+    },
+  },
+  build: {
+    outDir: 'dist', // ✅ this is required for Netlify
+  },
+  plugins: [svgr(), react()],
+});
